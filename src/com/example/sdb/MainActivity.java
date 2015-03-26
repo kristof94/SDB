cd9a11273088252e37e225fbfcf66d7883268446
master <master@master-HP-ENVY-17-Notebook-PC>
2015-03-26 02:33:52 +0100

    oook

--- src/com/example/sdb/MainActivity.java
+++ src/com/example/sdb/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.sdb;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Collection;
-
-import javax.mail.MessagingException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hld.mht.WebViewActivity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -21,69 +23,58 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
-import com.hankenindustries.mht.Attachment;
-import com.hankenindustries.mht.MHTUnpack;
-
 public class MainActivity extends Activity {
 
 	private static final int CHOOSE_FILE_REQUESTCODE = 1;
 	LinearLayout layout;
-	//List<String> PATH,NAME;
+	List<String> PATH,NAME;
 	String state;
 	static File Index;
 	private boolean isShow;
-	//File file;
-	//	FileOutputStream outputStream;
-	//FileInputStream fin ;
+	Intent i ;
+	static String FOLDER;
+
+	File sauvegarde;
+	FileInputStream fin ;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//PATH = new ArrayList<String>();
-		//NAME = new ArrayList<String>();
-
-		/*file = new File(this.getFilesDir(), "PATH");
-		try {
-			fin = openFileInput("PATH");
-			BufferedReader br = new BufferedReader(new InputStreamReader(fin));
-			String line = null;
-			List<String> spl= new ArrayList<String>();
-			int y =0;
-			while ((line = br.readLine()) != null) {
-				spl.add(line);
-				if(y!=0)
-					{
-						this.add_button(spl.get(y-1), spl.get(y));
-						//System.out.println(PATH.get(y-1));
-					}
-				y++;
-
-			}
-
-
-
-
-			fin.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		 */
 		setContentView(R.layout.activity_main);
 		layout = (LinearLayout) findViewById(R.id.ll);
+		i = new Intent(this,Loading.class);
 		String state = Environment.getExternalStorageState();
-		//if(!Environment.MEDIA_MOUNTED.equals(state)) {
 		if(Environment.MEDIA_MOUNTED.equals(state)) {
 			Log.d("SDB", "yes");
 		} else {
 			Log.d("SDB", "no");
 		}
+		PATH = new ArrayList<String>();
+		NAME = new ArrayList<String>();
+		FOLDER = this.getFilesDir().getAbsolutePath();
+		String line = null,l2[];
 
-		//} else {
-		//;
-		//}
+
+		try {
+			fin = openFileInput("PATH");
+			BufferedReader br = new BufferedReader(new InputStreamReader(fin));
+			fin = openFileInput("PATH");
+			while ((line = br.readLine()) != null) {
+				l2 = line.split("-");
+				PATH.add(l2[0]);
+				NAME.add(l2[1]);
+				System.out.println(l2[0]);
+				System.out.println(l2[1]);
+				this.add_button(l2[0], l2[1]);
+			}
+			fin.close();
+
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
@@ -104,11 +95,17 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	private void add_button(String PAT,final String name)
 	{
 		final String P= PAT;
 		Button b = new Button(this);
+
+		if(!PATH.contains(PAT))
+			PATH.add(PAT);
+		if(!NAME.contains(name))			
+			NAME.add(name);
+
 		b.setText(name.replace(".mht",""));
 		b.setOnClickListener(new OnClickListener(){
 
@@ -121,7 +118,7 @@ public class MainActivity extends Activity {
 
 		layout.addView(b);
 	}
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		switch(requestCode){
@@ -142,7 +139,7 @@ public class MainActivity extends Activity {
 	private void open_mht(String p,String name) {
 		Log.d("SDB",p);
 		Log.d("SDB",name);
-		File folder = new File(this.getFilesDir().getAbsolutePath()+"/"+name.replace(".mht", ""));
+		File folder = new File(FOLDER+"/"+name.replace(".mht", ""));
 		File Index_HTML = new File(folder.getAbsolutePath()+"/"+"index.html");
 
 		if(folder.exists())
@@ -156,9 +153,13 @@ public class MainActivity extends Activity {
 			}
 			else
 			{
-				Extract_MHT o =new Extract_MHT(new File(p+name),folder.getAbsolutePath()+"/",this);
+				//Extract_MHT o =new Extract_MHT(new File(p+name),folder.getAbsolutePath()+"/",this);
 				this.add_button(folder.getAbsolutePath()+"/",name);
-				o.execute();
+				i.putExtra("file",p+name );
+				i.putExtra("folder",folder.getAbsolutePath()+"/");
+				startActivity(i);
+
+				//o.execute();
 			}
 		}
 		else
@@ -166,9 +167,14 @@ public class MainActivity extends Activity {
 			Log.d("SDB","Folder "+p+" not created.");
 			folder.mkdir();
 			Log.d("SDB","Folder "+p+" created");
-			Extract_MHT o =new Extract_MHT(new File(p+name),folder.getAbsolutePath()+"/",this);
+			//Extract_MHT o =new Extract_MHT(new File(p+name),folder.getAbsolutePath()+"/",this);
+
 			this.add_button(folder.getAbsolutePath()+"/",name);
-			o.execute();
+			i.putExtra("file",p+name );
+			i.putExtra("folder",folder.getAbsolutePath()+"/");
+			//this.add_button(folder.getAbsolutePath()+"/",name);
+			startActivity(i);
+			//			o.execute();
 		}
 
 	}
@@ -201,30 +207,20 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onStop() {
 
-		/*if(!PATH.isEmpty())
+		String chemin;
+
+		try {
+			FileOutputStream outputStream = openFileOutput("PATH", this.MODE_PRIVATE);
+			for(int i =0;i<PATH.size();i++)
 			{
-				try {
-					outputStream = openFileOutput("PATH", this.MODE_PRIVATE);
-					String data;
-					for(int i = 0;i<PATH.size();i++)
-					{
-						data = PATH.get(i)+System.getProperty("line-separator")+NAME.get(i)+System.getProperty("line-separator");
-						Log.d("SDB", data);
-						outputStream.write(data.getBytes());
-
-					}
-					Log.d("SDB", file.getAbsolutePath());
-					outputStream.close();
-
-				} catch (FileNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-			}*/
+				chemin = PATH.get(i)+"-"+NAME.get(i)+System.getProperty("line.separator");
+				outputStream.write(chemin.getBytes());
+			}
+			outputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 
 		super.onStop();
@@ -248,6 +244,8 @@ public class MainActivity extends Activity {
 		Intent intent = new Intent();
 		intent.setClass(activity, WebViewActivity.class);
 		intent.putExtra("path", filePath);
+		intent.putExtra("folder",FOLDER );
+
 		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		return intent;
 	}
@@ -256,96 +254,5 @@ public class MainActivity extends Activity {
 		activity.startActivity(createShowHtmlIntent(activity, filePath));
 	}
 
-	public class Extract_MHT extends AsyncTask<Void, Void, String>{
 
-		Collection<Attachment> at;
-		File MHT;
-		String path;
-		Activity aa;
-
-		public Extract_MHT(File mht,String Path,Activity a) {
-
-			aa=a;
-			MHT = mht;
-			path = Path;
-			this.at = null;
-			Log.d("SDB","CONSTRUCTOR "+MHT.getAbsolutePath());
-			Log.d("SDB","CONSTRUCTOR "+path);
-
-		}
-
-		@Override
-		protected void onPreExecute() {
-			setContentView(R.layout.loading);
-			Log.d("SDB","set loading");
-
-		}
-
-		@Override
-		protected void onPostExecute(String result) {
-			super.onPostExecute(result);
-			setContentView(R.layout.activity_main);
-			MainActivity.showHtml(aa, Index.getAbsolutePath());
-		}
-
-		@Override
-		protected String doInBackground(Void... params) {
-			try {
-				at = MHTUnpack.unpack(MHT);
-			} catch (MessagingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-			String name="";
-			Log.d("SDB",at.size()+" Fichiers extraits.");
-			for(Attachment a : at)
-			{
-				try {
-					name = a.getFileName();
-				} catch (MessagingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				if(name != null)
-				{
-					File file = new File(path+name);
-					File parent = file.getParentFile();
-					if(!parent.exists() && !parent.mkdirs())
-						System.err.println("Couldn't create dir: " + parent);
-					try {
-						a.saveFile(file);
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (MessagingException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				else
-				{
-					Index = new File(path+"index.html");
-					File parent = Index.getParentFile();
-					if(!parent.exists() && !parent.mkdirs())
-						System.err.println("Couldn't create dir: " + parent);
-					try {
-						a.saveFile(Index);
-						System.out.println(Index.getAbsolutePath());
-						System.out.println("Termin�");
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (MessagingException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			}
-			return null;
-		}	
-	}
 }
--- src/org/hld/mht/WebViewActivity.java
+++ src/org/hld/mht/WebViewActivity.java
@@ -1,22 +1,28 @@
 package org.hld.mht;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.Window;
-import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.sdb.R;
 
 public class WebViewActivity extends Activity {
 	WebView webView ;
-
+	String path,folder;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,13 +31,16 @@ public class WebViewActivity extends Activity {
 		webView = (WebView)findViewById(R.id.WebView);
 
 		//
-		
+
 
 		Intent intent = getIntent();
 		if(intent==null) {
 			showMessage("test");
 		} else {
-			final String path = intent.getExtras().getString("path");
+			path = intent.getExtras().getString("path");
+			folder = intent.getExtras().getString("folder");
+			Log.d("SSDB","path "+path + " folder "+folder);
+
 			if(path==null) {
 				showMessage("test2");
 			} else if(!new File(path).isFile()) {
@@ -41,28 +50,25 @@ public class WebViewActivity extends Activity {
 				webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
 				webView.getSettings().setJavaScriptEnabled(true);
 				webView.getSettings().setLoadsImagesAutomatically(true);
-				
+				webView.setDrawingCacheEnabled(true);
 				//active le zoom
-			    webView.getSettings().setBuiltInZoomControls(true);
-			    webView.getSettings().setDisplayZoomControls(false);
-			    webView.getSettings().setUseWideViewPort(true);
-			    webView.setInitialScale(1);
-			    webView.getSettings().setLoadWithOverviewMode(true);
+				webView.getSettings().setBuiltInZoomControls(true);
+				webView.getSettings().setDisplayZoomControls(false);
+				webView.getSettings().setUseWideViewPort(true);
+				webView.setInitialScale(1);
+				webView.getSettings().setLoadWithOverviewMode(true);
 
-				
-				webView.setWebChromeClient(new WebChromeClient() {
+
+				webView.setWebViewClient(new WebViewClient(){
+
 					@Override
-					public void onProgressChanged(WebView view, int newProgress) {
-						WebViewActivity.this.setProgress(newProgress * 100);
+					public void onPageFinished(WebView view, String url) {
+						
+								WebViewActivity.this.setTitle("TDB");
+								
+						super.onPageFinished(view, url);
 					}
-					@Override
-					public void onReceivedTitle(WebView view, String title) {
-						WebViewActivity.this.setTitle(title);
-					}
-					@Override
-					public void onRequestFocus(WebView view) {
-						super.onRequestFocus(view);
-					}
+					
 				});
 				webView.loadUrl("file://"+Uri.encode(path, "/\\"));
 				Log.d("SSDB","file://"+Uri.encode(path, "/\\") );
@@ -70,16 +76,74 @@ public class WebViewActivity extends Activity {
 		}
 
 	}
-	
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if(keyCode == KeyEvent.KEYCODE_BACK) {
+			/*new Runnable() {
+				public void run() {
+					try {
+						take_snap(loadBitmapFromView(
+								webView,
+								webView.getWidth(),
+								webView.getHeight()));
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}.run();*/
 			finish();
-    		return true;
-    	}
-    	return super.onKeyDown(keyCode, event);
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
 	}
-	
+
+	public static Bitmap loadBitmapFromView(View v, int width, int height) {
+		Bitmap b = Bitmap.createBitmap(width , height, Bitmap.Config.ARGB_8888);                
+		Canvas c = new Canvas(b);
+		v.layout(0, 0, v.getLayoutParams().width/3, v.getLayoutParams().height/4);
+		v.draw(c);
+		return b;
+	}
+
+	public void take_snap(Bitmap bmp) throws IOException
+	{
+		File f = new File("/sdcard/Download/"+"pict.png");
+		f.createNewFile();
+		FileOutputStream fOut;
+		try {
+			fOut = new FileOutputStream(folder+"/"+"pict.png");
+			bmp.compress(Bitmap.CompressFormat.PNG, 85, fOut);		    
+			fOut.flush();
+			fOut.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void save_file(){
+		String str = folder+"/"+"pict.png";
+
+		try {
+			FileOutputStream outputStream = openFileOutput("PATH", this.MODE_PRIVATE);
+
+			outputStream.write(str.getBytes());
+
+			outputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		Log.d("SSDB","snap "+str );
+	}
+
+
 	private void showMessage(String msg) {
 		webView.getSettings().setDefaultTextEncodingName("UTF-8");
 		webView.loadData(Uri.encode(msg, "UTF-8"), "text/plain", "UTF-8");
